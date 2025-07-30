060598430c7268b3d7f61b9f22956512d0f145d6
YCC77 <camargo7yatziri@gmail.com>
2025-07-29 19:02:28 -0600

    Primera versao

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+import asyncio
+from cloudflarify import start_tunnel
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("pages/index.html")
+
+@app.route('/login' , methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template("pages/login.html")
+    else:
+        return redirect(url_for('index'))
+
+print("URL:" + asyncio.run(start_tunnel(app_port=5000)))
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
